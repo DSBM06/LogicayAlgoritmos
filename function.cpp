2d8dd419129d120d7bd467ef7d065a42628559df
DSBM06 <dbrenes@uamv.edu.ni>
2024-05-17 08:23:44 -0600

    se agregó sesion1705

---
+++ function.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+//Prototipo de funciones
+void menu();
+
+
+//Funciones
+void menu(){
+    system("cls||clear");
+    cout<<"1. Ingresar edades"<<endl;
+    cout<<"2. Mostrar edades"<<endl;
+    cout<<"3. Mostrar edad mayor"<<endl;
+    cout<<"4. Mostrar edad menor"<<endl;
+    cout<<"5. Mostrar promedio de edades"<<endl;
+    cout<<"6. Salir"<<endl;
+}
