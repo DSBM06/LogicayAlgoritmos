f369ab1c1e88850d18c88e95316eac2523d1aaaa
DSBM06 <dbrenes@uamv.edu.ni>
2024-05-17 08:51:45 -0600

    se agrego sesion1705

--- function.cpp
+++ function.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
+#include"variable.h"
 
 using namespace std;
 
 //Prototipo de funciones
+void start();
 void menu();
+void addAges();
+void mostAges();
 
 
 //Funciones
@@ -15,4 +19,48 @@ void menu(){
     cout<<"4. Mostrar edad menor"<<endl;
     cout<<"5. Mostrar promedio de edades"<<endl;
     cout<<"6. Salir"<<endl;
+}
+
+//Agregar edades
+void addAges(){
+    system("cls||clear");
+    cout<< "Dime la edad: ";
+    cin >> ages[pos];
+    pos++;
+    system("pause");
+}
+
+void start(){
+    int option;
+
+    while(option!=6){
+        menu();
+        cin>>option;
+        switch (option)
+        {
+        case 1:
+            addAges();
+            break;
+
+            case 2:
+            mostAges();
+            break;
+
+        case 6:
+            cout<<"Adios"<< endl;
+            break;
+        
+        default:
+            break;
+        }
+    }
+}
+
+void mostAges(){
+    system("cls||clear");
+    cout<<"edades: "<<endl;
+    for (int i = 0; i < pos; i++){
+        cout<<ages[i]<<endl;
+    }
+    system("pause");
 }
--- principal.cpp
+++ principal.cpp
@@ -2,15 +2,11 @@
 #include "function.cpp"
 
 //Realice un programa que permita almacenar x cantidad de edades, mostrar todas las edades, la edad mayor, la edad menor y el promedio de edades//
+
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int op;
-
-    while(op!=6){
-        menu();
-        cin>>op;
-    }
+    start();
     return 0;
 }
